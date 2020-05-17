0d4db4162a598afadc6ed41a4af223d4ac4e08af
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2020-05-16 18:33:00 -0700

    Rewrite to follow new best practices

--- src/main.ts
+++ src/main.ts
@@ -1,69 +1,15 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
-const query = `
-query($searchQuery: String!) {
-  search(first: 1, query: $searchQuery, type: ISSUE) {
-    nodes {
-      ... on Issue {
-        assignees(first: 1) {
-          nodes {
-            login
-          }
-        }
-      }
-      ... on PullRequest {
-        assignees(first: 1) {
-          nodes {
-            login
-          }
-        }
-      }
-    }
-  }
-}
-`
-
-function formatNameWithOwner({ owner, repo }) {
-  return `${owner}/${repo}`
-}
-
-async function getLastAssigned(octokit, searchQuery) {
-  const queryText = `repo:${formatNameWithOwner(github.context.repo)} sort:created ${searchQuery}`
-
-  core.debug(`Query: ${queryText}`)
-
-  const results = await octokit.graphql(query, { searchQuery: queryText })
-
-  core.debug(`Results: ${JSON.stringify(results)}`)
-
-  if (results.search.nodes.length == 0) {
-    return null
-  }
-
-  if (results.search.nodes[0].assignees.nodes.length == 0) {
-    return null
-  }
-
-  return results.search.nodes[0].assignees.nodes[0].login
-}
+import { getLastAssigned } from './query'
 
 async function run() {
   try {
-    const query = core.getInput('query')
-
-    if (!query) {
-      throw new Error('`query` is required')
-    }
-
-    const token = core.getInput('token')
-
-    if (!token) {
-      throw new Error('`token` is required')
-    }
+    const query = core.getInput('query', { required: true })
+    const token = core.getInput('token', { required: true })
 
     const octokit = new github.GitHub(token)
-    const assigned = await getLastAssigned(octokit, query)
+    const assigned = (await getLastAssigned(octokit, query)) ?? ''
     core.debug(`Last assigned: ${assigned}`)
 
     core.setOutput('last', assigned)
---
+++ test/query.test.ts
@@ -0,0 +1,106 @@
+import nock from 'nock'
+
+import { getLastAssigned, SearchQueryResponse } from '../src/query'
+import { GitHub } from '@actions/github'
+
+let requestBody: nock.Body
+
+interface ReturnValue {
+  data: SearchQueryResponse
+}
+
+function graphqlNock(returnValue: ReturnValue) {
+  nock('https://api.github.com')
+    .post('/graphql')
+    .reply(200, (_, body) => {
+      requestBody = body
+
+      return returnValue
+    })
+}
+
+describe('getLastAssigned', () => {
+  const mockToken = '1234567890abcdef'
+  const testQuery = 'label:weekly-issue'
+
+  let octokit: GitHub
+
+  beforeEach(() => {
+    Object.assign(process.env, {
+      GITHUB_REPOSITORY: 'test-owner/test-repo',
+      GITHUB_ACTION: 'last-assigned'
+    })
+
+    octokit = new GitHub(mockToken)
+  })
+
+  it('returns the login of the assignee', async () => {
+    graphqlNock({
+      data: {
+        search: {
+          nodes: [
+            {
+              assignees: {
+                nodes: [
+                  {
+                    login: 'test-user'
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      }
+    })
+
+    const login = await getLastAssigned(octokit, testQuery)
+
+    expect((requestBody as Record<string, any>).variables.searchQuery).toBe(
+      `repo:test-owner/test-repo sort:created ${testQuery}`
+    )
+
+    expect(login).toBe('test-user')
+  })
+
+  it('returns null when nothing is found', async () => {
+    graphqlNock({
+      data: {
+        search: {
+          nodes: []
+        }
+      }
+    })
+
+    const login = await getLastAssigned(octokit, testQuery)
+
+    expect((requestBody as Record<string, any>).variables.searchQuery).toBe(
+      `repo:test-owner/test-repo sort:created ${testQuery}`
+    )
+
+    expect(login).toBeNull()
+  })
+
+  it('returns null when no assignees were found', async () => {
+    graphqlNock({
+      data: {
+        search: {
+          nodes: [
+            {
+              assignees: {
+                nodes: []
+              }
+            }
+          ]
+        }
+      }
+    })
+
+    const login = await getLastAssigned(octokit, testQuery)
+
+    expect((requestBody as Record<string, any>).variables.searchQuery).toBe(
+      `repo:test-owner/test-repo sort:created ${testQuery}`
+    )
+
+    expect(login).toBe(null)
+  })
+})
